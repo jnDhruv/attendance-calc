f57513042d1f2037ac1196c7fc8bf4a5c9c220f4
Dhruv <dhruvjain.dev@gmail.com>
2025-07-20 10:25:30 +0530

    feat: create Attendance class

---
+++ src/scripts/Attendance.js
@@ -0,0 +1,51 @@
+export default class Attendance {
+  constructor(presentSlots = 0, totalSlots = 0, goal = 75) {
+    this.initial = [presentSlots, totalSlots];
+    this.current = [presentSlots, totalSlots];
+    this.goal = goal;
+  }
+
+  attendSlots(x = 1) {
+    this.updateAttendance(this.current[0] + x, this.current[1] + x);
+  }
+
+  missSlots(x = 1) {
+    this.updateAttendance(this.current[0], this.current[1] + x);
+  }
+
+  updateAttendance(newPresent, newTotal) {
+    this.current = [newPresent, newTotal];
+  }
+  
+  resetUpdated() {
+    this.current[0] = this.initial[0];
+    this.current[1] = this.initial[1];
+  }
+
+  getPercentage(fromArr = this.current) {
+    return Math.round((fromArr[0] / fromArr[1]) * 100 * 100) / 100;
+  }
+
+  getCanBeMissed(fromArr = this.initial) {
+    let skipped = 0;
+    let present = fromArr[0];
+    let total = fromArr[1];
+    while ((present / total) * 100 >= this.goal) {
+      total++;
+      skipped++;
+    }
+    return skipped - 1;
+  }
+  
+  getNeedToAttend(fromArr = this.initial) {
+    let attended = 0;
+    let present = fromArr[0];
+    let total = fromArr[1];
+    while ((present / total) * 100 < this.goal) {
+      present++
+      total++;
+      attended++;
+    }
+    return attended;
+  }
+}
